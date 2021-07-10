501017c1d1603443ffd7109c148d8ab11462369e
Chris Irish <irish@velocitylabs.io>
2021-07-09 18:53:44 -0700

    make starburst js work with turbolinks

--- app/assets/javascripts/starburst/starburst.js
+++ app/assets/javascripts/starburst/starburst.js
@@ -1,19 +1,12 @@
-function ready(fn) {
-  if (document.addEventListener) {
-    document.addEventListener('DOMContentLoaded', fn);
-  } else {
-    document.attachEvent('onreadystatechange', function() {
-      if (document.readyState === 'interactive')
-        fn();
-    });
-  }
-}
 
-ready(function addOneTimeMessagesCallbacks() {
+function addOneTimeMessagesCallbacks() {
 	var closeButton = document.getElementById('starburst-close');
+
 	if (closeButton !== null) {
 		closeButton.addEventListener('click', function() {
 			document.getElementById('starburst-announcement').style.display = 'none';
 		});
 	}
-});
+};
+
+$(document).on( "turbolinks:load", addOneTimeMessagesCallbacks);
